5d3a60a85ec4dfe37d8357f90ee8bedf3db8a901
GhoZaa <ghoza16m@gmail.com>
2021-06-28 14:44:07 +0700

    edit static value

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   BASE_URL: "https://restaurant-api.dicoding.dev/",
   BASE_IMAGE_URL: "https://restaurant-api.dicoding.dev/images/medium/",
-  CAHCE_NAME: "RestoApps-" + new Date().toISOString(),
+  CAHCE_NAME: "RestoApps-v1",
   DATABASE_NAME: "resto-app-db",
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: "restaurants",
