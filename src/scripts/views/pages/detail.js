d91007394bde197a5432c20e0c023d52d85aefb4
GhoZaa <ghoza16m@gmail.com>
2021-06-28 10:56:39 +0700

    giving functionality to the like button

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,20 +1,39 @@
 import Source from "../../data/source";
 import UrlParser from "../../routes/url-parser";
-import { createRestaurantDetailTemplate } from "../templates/template-creator";
+import LikeButtonInitiator from "../../utils/like-button-initiator";
+import {
+  createLikeButtonTemplate,
+  createRestaurantDetailTemplate,
+} from "../templates/template-creator";
 
 const Detail = {
   async render() {
     return `
-    <div id="detail-restaurant"></div>
-      `;
+      <div id="detail-restaurant"></div>
+      <div id="likeButtonContainer"></div>
+    `;
   },
 
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const restaurant = await Source.detailRestaurant(url.id);
     // console.log(restaurant);
-    const restaurantContainer = document.querySelector('#detail-restaurant');
+    const restaurantContainer = document.querySelector("#detail-restaurant");
     restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
+
+    // const likeButtonContainer = document.querySelector('#likeButtonContainer');
+    // likeButtonContainer.innerHTML = createLikeButtonTemplate();
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector("#likeButtonContainer"),
+      restaurant: {
+        id: restaurant.id,
+        name: restaurant.name,
+        description: restaurant.description,
+        city: restaurant.city,
+        picturId: restaurant.picturId,
+        rating: restaurant.rating,
+      },
+    });
   },
 };
 
