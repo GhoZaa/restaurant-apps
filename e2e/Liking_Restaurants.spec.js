30e1004627368edcf98e6c82b7b53d369ca037b2
GhoZaa <ghoza16m@gmail.com>
2021-07-16 11:24:07 +0700

    create e2e test 1: liking a restaurant

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 Feature('Liking Restaurants');
 
 Before(({ I }) => {
@@ -10,9 +12,24 @@ Scenario('showing empty liked restaurants', ({ I }) => {
   I.see('You don\'t have a favorite restaurant yet', '.description');
 });
 
-Scenario('liking one restaurant', ({ I }) => {
+Scenario('liking one restaurant', async ({ I }) => {
   I.see('List is empty!');
   I.see('You don\'t have a favorite restaurant yet', '.description');
 
   I.amOnPage('/');
+
+  I.seeElement('.item__title a');
+
+  const firstRestaurant = locate('.item__title a').first();
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.list__item');
+  const favoritedRestaurantName = await I.grabTextFrom('.item__title');
+
+  assert.strictEqual(firstRestaurantName, favoritedRestaurantName);
 });
