99d097a4e70650423c7f90a02f55be29a3fd4d0e
GhoZaa <ghoza16m@gmail.com>
2021-07-16 10:32:17 +0700

    create and run first e2e testing

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,5 +1,18 @@
 Feature('Liking Restaurants');
 
-Scenario('test something', ({ I }) => {
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+});
 
+Scenario('showing empty liked restaurants', ({ I }) => {
+  I.seeElement('#empty-list');
+  I.see('List is empty!');
+  I.see('You don\'t have a favorite restaurant yet', '.description');
+});
+
+Scenario('liking one restaurant', ({ I }) => {
+  I.see('List is empty!');
+  I.see('You don\'t have a favorite restaurant yet', '.description');
+
+  I.amOnPage('/');
 });
